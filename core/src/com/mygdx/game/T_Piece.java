62d2473bddf4c5b21e8a2955e1c950697dd9d453
AmbroiseS <amb.soullier@gmail.com>
2017-01-04 17:23:39 +0100

    first ideas for piece management

--- core/src/com/mygdx/game/T_Piece.java
+++ core/src/com/mygdx/game/T_Piece.java
@@ -11,39 +11,98 @@ import com.badlogic.gdx.utils.TimeUtils;
 
 public class T_Piece {
 
-    Rectangle t_piece;
+    private Rectangle t_piece;
+    private int pieceRotation = 0;
+    private int squareSize;
 
-    public T_Piece(){
-        t_piece=new Rectangle();
-        t_piece.x = 50 + 5 * 15;
-        t_piece.y = 50 + 19 * 45;
+    public T_Piece() {
+        squareSize=MyGdxGame.squareSize;
+        t_piece = new Rectangle();
+        t_piece.x = 50 + 5 * squareSize;
+        t_piece.y = 50 + 19 * squareSize;
+
 
 
     }
-    public void update(){
-        MyGdxGame.batch.draw(MyGdxGame.unit_texture,t_piece.x,t_piece.y,15,45);
-        MyGdxGame.batch.draw(MyGdxGame.unit_texture,t_piece.x-15,t_piece.y-45,15,45);
-        MyGdxGame.batch.draw(MyGdxGame.unit_texture,t_piece.x,t_piece.y-45,15,45);
-        MyGdxGame.batch.draw(MyGdxGame.unit_texture,t_piece.x+15,t_piece.y-45,15,45);
+
+
+    public void update() {
+        switch (pieceRotation) {
+            //each case for a rotation
+
+            case (0):
+
+                MyGdxGame.batch.draw(MyGdxGame.unit_texture, t_piece.x, t_piece.y, squareSize, squareSize);
+                MyGdxGame.batch.draw(MyGdxGame.unit_texture, t_piece.x - squareSize, t_piece.y - squareSize, squareSize, squareSize);
+                MyGdxGame.batch.draw(MyGdxGame.unit_texture, t_piece.x, t_piece.y - squareSize, squareSize, squareSize);
+                MyGdxGame.batch.draw(MyGdxGame.unit_texture, t_piece.x + squareSize, t_piece.y - squareSize, squareSize, squareSize);
+                break;
+
+            case (1):
+
+                MyGdxGame.batch.draw(MyGdxGame.unit_texture, t_piece.x, t_piece.y, squareSize, squareSize);
+                MyGdxGame.batch.draw(MyGdxGame.unit_texture, t_piece.x, t_piece.y - squareSize * 2, squareSize, squareSize);
+                MyGdxGame.batch.draw(MyGdxGame.unit_texture, t_piece.x, t_piece.y - squareSize, squareSize, squareSize);
+                MyGdxGame.batch.draw(MyGdxGame.unit_texture, t_piece.x + squareSize, t_piece.y - squareSize, squareSize, squareSize);
+                break;
+
+
+            case (2):
+
+                MyGdxGame.batch.draw(MyGdxGame.unit_texture, t_piece.x - squareSize, t_piece.y - squareSize, squareSize, squareSize);
+                MyGdxGame.batch.draw(MyGdxGame.unit_texture, t_piece.x, t_piece.y - squareSize * 2, squareSize, squareSize);
+                MyGdxGame.batch.draw(MyGdxGame.unit_texture, t_piece.x, t_piece.y - squareSize, squareSize, squareSize);
+                MyGdxGame.batch.draw(MyGdxGame.unit_texture, t_piece.x + squareSize, t_piece.y - squareSize, squareSize, squareSize);
+                break;
+
+            case (3):
+
+                MyGdxGame.batch.draw(MyGdxGame.unit_texture, t_piece.x - squareSize, t_piece.y - squareSize, squareSize, squareSize);
+                MyGdxGame.batch.draw(MyGdxGame.unit_texture, t_piece.x, t_piece.y - squareSize * 2, squareSize, squareSize);
+                MyGdxGame.batch.draw(MyGdxGame.unit_texture, t_piece.x, t_piece.y - squareSize, squareSize, squareSize);
+                MyGdxGame.batch.draw(MyGdxGame.unit_texture, t_piece.x, t_piece.y, squareSize, squareSize);
+                break;
+
+        }
     }
 
+    long timeDown = 0;
     long timeLeft = 0;
     long timeRight = 0;
+    long timeRotate = 0;
     long repeatTimeMillis = 90;
 
-    public void inputUpdate(){
+    public void inputUpdate() {
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT) && TimeUtils.millis() - timeLeft > repeatTimeMillis) {
-            if (t_piece.x > 65) {
-                t_piece.x -= 15;
+            if (t_piece.x > 50+squareSize) {
+                t_piece.x -= squareSize;
                 timeLeft = TimeUtils.millis();
             }
 
         }
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) && TimeUtils.millis() - timeRight > repeatTimeMillis) {
-            if (t_piece.x < 50-15 + 9 * 15)
-                t_piece.x += 15;
-            timeRight = TimeUtils.millis();
+            if (t_piece.x < 50 -squareSize + 9 * squareSize) {
+                t_piece.x += squareSize;
+                timeRight = TimeUtils.millis();
+            }
         }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) && TimeUtils.millis() - timeRotate > repeatTimeMillis) {
+            if (pieceRotation == 3) {
+                pieceRotation = 0;
+                timeRotate = TimeUtils.millis();
+            } else {
+                pieceRotation += 1;
+                timeRotate = TimeUtils.millis();
+            }
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) && TimeUtils.millis() - timeDown > repeatTimeMillis) {
+            if (t_piece.y > 50+squareSize ) {
+                t_piece.y -= squareSize;
+                timeDown = TimeUtils.millis();
+            }
 
+        }
     }
 }
+
+
